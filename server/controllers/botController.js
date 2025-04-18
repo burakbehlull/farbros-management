37b95cd32f4559d963f053341f0fe036f77ba76c
burak behlül <burakyabgu@gmail.com>
2025-04-18 20:26:16 +0300

    feat: Add new bot stop feature

--- server/controllers/botController.js
+++ server/controllers/botController.js
@@ -1,9 +1,17 @@
 import { bot } from "../index.js"
 import Bot from "../models/Bot.js"
+
+const botList = []
+
 const BotStart = async (req, res)=>{
     const { token } = req.body
+    
+    if (botList.find(b => b.token === token)) return res.json({ status: true, message: "Bu bot zaten çalışıyor." })
+
     try {
         await bot.login(token)
+        botList.push({ token, client: bot })
+
         res.json({
             status: true,
             message: `Bot bağlandı: ${client.user.tag}` 
@@ -16,6 +24,21 @@ const BotStart = async (req, res)=>{
             message: "Bot giriş yapmadı"
         })
     }
+}
+
+const BotStop = async (req, res) => {
+    const { token } = req.body;
+
+    const index = botList.findIndex(b => b.token === token)
+    if (index === -1) return res.json({ message: "Bu tokena ait çalışan bot yok." })
+
+    await botList[index].client.destroy()
+    botList.splice(index, 1)
+
+    return res.json({ 
+        status: true,
+        message: "Bot durduruldu." 
+    })
 }
 
 const BotAdd = async (req,res)=> {
@@ -38,6 +61,7 @@ const GetBots = async(req,res)=> {
 
 export {
     BotStart,
+    BotStop,
     BotAdd,
     GetBots
 }
