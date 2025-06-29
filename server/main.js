bdac4b36f43a4b90b0ffc452643d2cdd19d1ae69
burak behlül <burakyabgu@gmail.com>
2025-06-29 13:53:05 +0300

    feat: Add code skeleton

--- server/main.js
+++ server/main.js
@@ -1,1 +1,23 @@
-console.log(1)
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) => {
+  res.send('hello farbros managament');
+});
+
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+  console.log(`Server ${PORT} portunda çalışıyor.`);
+});
+
+
