0d0e81d8ea31e75973d862287450de20991f4f5e
burak behlül <burakyabgu@gmail.com>
2025-08-20 14:24:04 +0300

    feat: Add bot feature functions

--- server/services/botFeatureService.js
+++ server/services/botFeatureService.js
@@ -1,7 +1,17 @@
 import { BotFeature } from "#models";
 
+const addBotFeature = async (botFeatureData) => {
+    try {
+        const newBotFeature = new BotFeature(botFeatureData);
+        await newBotFeature.save();
+        return newBotFeature;
+    } catch (error) {
+        console.error("[addBotFeature] Error adding bot feature:", error);
+        throw new Error("Error adding bot feature");
+    }
+}
 
-const getByBotId = async (botId) => {
+const getFeaturesByBotId = async (botId) => {
     try {
         const botFeatures = await BotFeature.find({ bot: botId }).populate("feature");
         if (!botFeatures || botFeatures.length === 0) {
@@ -12,5 +22,28 @@ const getByBotId = async (botId) => {
         console.error(`[getByBotId] Error fetching features for bot with ID ${botId}:`, error);
         throw new Error(`Error fetching features for bot with ID ${botId}`);
     }
-    
+
 }
+
+const setFeatureStatus = async (panelId, status) => {
+    try {
+        const updatedBotFeature = await BotFeature.findOneAndUpdate(
+            { panelId },
+            { status },
+            { new: true, runValidators: true }
+        );
+        if (!updatedBotFeature) {
+            throw new Error(`Bot feature with panelId ${panelId} not found`);
+        }
+        return updatedBotFeature;
+    } catch (error) {
+        console.error(`[setFeatureStatus] Error updating feature status for panelId ${panelId}:`, error);
+        throw new Error(`Error updating feature status for panelId ${panelId}`);
+    }
+};
+
+export {
+    addBotFeature,
+    getFeaturesByBotId,
+    setFeatureStatus
+};
