37cf8e9bb7e70ebc2b60e568b30b0985c42e72cc
burak behlul <burakyabgu@gmail.com>
2025-08-25 12:23:40 +0300

    fix: Fix feature controller and route features

--- server/controllers/featureController.js
+++ server/controllers/featureController.js
@@ -2,9 +2,22 @@
 
 import { featureService } from "#services";
 import { loadEvents, loadPrefixCommands, loadSlashCommands } from "#helpers";
+import { updateFeature } from "../services/featureService";
 
-const { addManyFeatures } = featureService
+const { addManyFeatures, addFeature, getFeatureList } = featureService
 
+// get all features
+const featureList = async (req, res) => {
+  try {
+    const features = await getFeatureList();
+    return res.status(200).json({ status: true, data: features });
+  } catch (err) {
+    console.error("[bot controller - Get Features]:", err);
+    return res.status(500).json({ message: "Özellikler getirilemedi.", error: err.message });
+  }
+}
+
+// upload features
 const uploadFeatures = async (req, res) => {
   try {
     const slashCommands = await loadSlashCommands();
@@ -20,6 +33,35 @@ const uploadFeatures = async (req, res) => {
   }
 };
 
+// upload one feature
+const addToFeature = async (req, res) => {
+  try {
+    const feature = req.body;
+    await addFeature(feature);
+    return res.status(201).json({ status: true, message: "Özellik eklendi." });
+  } catch (err) {
+    console.error("[bot controller - Add Feature]:", err);
+    return res.status(500).json({ message: "Özellik eklenemedi.", error: err.message });
+  }
+}
+
+// update one feature
+const updateToFeature = async (req, res) => {
+  try {
+    const feature = req.body;
+    await updateFeature(feature.panelId, feature);
+    return res.status(200).json({ status: true, message: "Özellik güncellendi." });
+  } catch (err) {
+    console.error("[bot controller - Update Feature]:", err);
+    return res.status(500).json({ message: "Özellik güncellenemedi.", error: err.message });
+  }
+}
+
+
+
 export {
-    uploadFeatures
+  featureList,
+  uploadFeatures,
+  addToFeature,
+  updateToFeature
 }
--- server/routers/featureRoute.js
+++ server/routers/featureRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { botList, uploadFeatures } from '#controllers'
+import { botList, uploadFeatures, addToFeature, updateToFeature, featureList } from '#controllers'
 
 const router = express.Router();
 
-router.get('/', (req,res)=> {
-	res.status(200).json({status: true, data: botList || []})
-});
+router.get('/', featureList);
 
-router.post("/sync", uploadFeatures)
+router.post("/sync", uploadFeatures);
+
+router.post("/add", addToFeature);
+router.put("/update", updateToFeature);
 
 
 export default router;
