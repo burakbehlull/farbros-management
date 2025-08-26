fc52520cf30fc7ebb2fd5297982a146408ef4ec0
burak behlul <burakyabgu@gmail.com>
2025-08-26 12:30:20 +0300

    feat: Add bot api endpoints in requests

--- client/src/helpers/requests.js
+++ client/src/helpers/requests.js
@@ -1,1 +1,20 @@
 // requests
+import API from '../base/api';
+
+
+const botAPI = {
+    bots: () => API.get('/bots'),
+    addBot: (data, config) => API.post('/bots', data, config),
+    startBot: (id) => API.post(`/bots/${id}/start`),
+    stopBot: (id) => API.post(`/bots/${id}/stop`),
+    deleteBot: (id) => API.delete(`/bots/${id}`),
+
+    updatePrefix: (id, data, config) => API.patch(`/bots/${id}/prefix`, data, config),
+    
+    reloadAll: (id) => API.post(`/bots/${id}/reload`),
+    reloadPrefixCommands: (id) => API.post(`/bots/${id}/reload/prefix`),
+    reloadSlashCommands: (id) => API.post(`/bots/${id}/reload/slash`),
+    reloadEvents: (id) => API.post(`/bots/${id}/reload/events`),
+};
+
+
