dcef5b360b756046a61eed7c4865d11db4506650
burak behlul <burakyabgu@gmail.com>
2025-11-14 21:25:31 +0300

    feat: Add create get messages control

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -35,18 +35,52 @@ const CreateMessage = async(req, res) => {
                 message.reply(genre)
         }
 
-        return {
+        return res.status(200).json({
             status: true,
             message: 'Mesaj işlemi başarılı.'
-        }
+        });
     } catch (error) {
-        return {
+        return res.status(500).json({
             status: false,
             message: 'Mesaj işlemi başarısız.',
             error: error.message
-        }
+        });
     }
 }
 
 
-export { CreateMessage };
+const GetMessages = async(req, res) => {
+    const { botId, userId, serverId, channelId, type } = req.body;
+    try {
+
+        const bot = await getBotById(botId);
+        if (!bot) return res.status(404).json({ message: "Bot bulunamadı." });
+
+        const index = botList.findIndex(b => b.token === bot.token);
+        if (index === -1) return res.status(400).json({ message: "Bot zaten çalışmıyor." });
+
+        const client = botList[index].client
+        const message = new Message(client, {
+            userId: userId,
+            serverId: serverId,
+            channelId: channelId
+        });
+        const messages = await message.getMessages(type);
+        return res.status(200).json({
+            status: true,
+            message: 'Mesajlar başarıyla alındı.',
+            data: messages
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: 'Mesajlar alınırken hata oluştu.',
+            error: error.message
+        });
+    }
+}
+
+export { 
+    CreateMessage, 
+    GetMessages
+};
